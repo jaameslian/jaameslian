c9357ce9ed48e6de988c5d1e8c4352142d5ed477
jaameslian <156141320+jaameslian@users.noreply.github.com>
2025-12-30 20:28:56 -0600

    Commit #2
    Added under construction display block: updated index.css, index.html, index.js. Created unreleased folder to hold the websites that are under construction, and created TESTING_Main.html, TESTING_Main.css, TESTING_Main.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+    var conText = document.getElementById("constructionText");
+
+    const originalText = conText.innerText;
+
+    conText.innerText = "";
+
+    for (let i = 0; i < originalText.length; i++) {
+        console.log(originalText.length);
+        setTimeout(() => {
+            conText.innerText = conText.innerText + originalText.charAt(i);
+        }, 100 * i);
+    }
 })
